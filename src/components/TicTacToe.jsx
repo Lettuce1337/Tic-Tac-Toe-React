8fb311b9b18598a1c38d606debc987b9e38d702b
Sirivarakul <sirivarakuljiropakarn@gmail.com>
2023-12-28 03:58:18 +0700

    Adjusted winner text style

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -81,7 +81,7 @@ function TicTacToe() {
             <div className='title'>
                 TicTacToe
             </div>
-            <div className="turn-counter" style={winner === null? {}:{display:'none'}}>Current turn = {turn%2===0? 'O':'X'}</div>
+            <div className="title" style={winner === null? {}:{display:'none'}}>Current turn = {turn%2===0? 'O':'X'}</div>
             <div className='title' style={winner === null? {display:'none'}:{}}>
                 {winner && winner === 'draw'? 'Nobody wins!':''}
                 {winner && winner !== 'draw'? `Winner is ${winner}!`:''}
